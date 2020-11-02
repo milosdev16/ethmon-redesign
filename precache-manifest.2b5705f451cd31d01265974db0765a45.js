47422be288be609cf0ec6db97a6563c18f8906a8
milosdev16 <milosducic80@gmail.com>
2020-11-02 12:27:29 -0800

    Updates

---
+++ precache-manifest.2b5705f451cd31d01265974db0765a45.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dafe92076c9009ccabcf8c566b91757",
+    "url": "/ethmon-redesign/index.html"
+  },
+  {
+    "revision": "ac06164997f63cfc520d",
+    "url": "/ethmon-redesign/static/css/main.e0bf1789.chunk.css"
+  },
+  {
+    "revision": "c55e3adedba3aa4ace35",
+    "url": "/ethmon-redesign/static/js/2.838ec3dc.chunk.js"
+  },
+  {
+    "revision": "20a6f33d71d567d1fe0dc1c77a1a22e7",
+    "url": "/ethmon-redesign/static/js/2.838ec3dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac06164997f63cfc520d",
+    "url": "/ethmon-redesign/static/js/main.cec24e2d.chunk.js"
+  },
+  {
+    "revision": "91e2e6cde767a837144e",
+    "url": "/ethmon-redesign/static/js/runtime-main.7c4af7ef.js"
+  },
+  {
+    "revision": "b65f8f5d2504393000cb0285fc862f54",
+    "url": "/ethmon-redesign/static/media/Adventure Off.b65f8f5d.png"
+  },
+  {
+    "revision": "324d641c9a48384d07d0b211445b12c2",
+    "url": "/ethmon-redesign/static/media/Adventure.324d641c.png"
+  },
+  {
+    "revision": "8a795c08b9e68ea3ea2486db29231381",
+    "url": "/ethmon-redesign/static/media/Back trees.8a795c08.png"
+  },
+  {
+    "revision": "caa873d887537aa4a35e307feb80c515",
+    "url": "/ethmon-redesign/static/media/Battle Off.caa873d8.png"
+  },
+  {
+    "revision": "53e9807a1dcbe6223c30938be05229ce",
+    "url": "/ethmon-redesign/static/media/Battle.53e9807a.png"
+  },
+  {
+    "revision": "85424dadbfd8b0923435c677169d15c7",
+    "url": "/ethmon-redesign/static/media/Bg.85424dad.png"
+  },
+  {
+    "revision": "e0c16c13384821e5e4928be0c9d59257",
+    "url": "/ethmon-redesign/static/media/Ground.e0c16c13.png"
+  },
+  {
+    "revision": "c1e777feb276fc9d41e3cfb111da224f",
+    "url": "/ethmon-redesign/static/media/Market Off.c1e777fe.png"
+  },
+  {
+    "revision": "e0fa3d14d6f4ec261c5899168d0d95d5",
+    "url": "/ethmon-redesign/static/media/Market.e0fa3d14.png"
+  },
+  {
+    "revision": "f6bf98e10f2faf1f3dfb560d44f0a57a",
+    "url": "/ethmon-redesign/static/media/Official store Off.f6bf98e1.png"
+  },
+  {
+    "revision": "56a84759bfffae227b359a4b9ae386c0",
+    "url": "/ethmon-redesign/static/media/Official store.56a84759.png"
+  },
+  {
+    "revision": "eef05b128897f51b34b6bfca1f37f676",
+    "url": "/ethmon-redesign/static/media/Stone.eef05b12.png"
+  },
+  {
+    "revision": "dbc410cef9913ab9b6c3caeb2f9e6014",
+    "url": "/ethmon-redesign/static/media/Tournament Off.dbc410ce.png"
+  },
+  {
+    "revision": "05d11ec05bbb2362028eec879d417016",
+    "url": "/ethmon-redesign/static/media/Tournament.05d11ec0.png"
+  },
+  {
+    "revision": "0e61700909c2064c3ff17ec0b123c4d6",
+    "url": "/ethmon-redesign/static/media/Trees Left Side.0e617009.png"
+  },
+  {
+    "revision": "cf74a314ad1a2712fcac73a27f04d09f",
+    "url": "/ethmon-redesign/static/media/Trees Right Side.cf74a314.png"
+  },
+  {
+    "revision": "00d7b6d94371a8a31bcfa9f64fd2b8b1",
+    "url": "/ethmon-redesign/static/media/china.00d7b6d9.png"
+  },
+  {
+    "revision": "79bcd7a62097312e88395c9322828485",
+    "url": "/ethmon-redesign/static/media/discord.79bcd7a6.png"
+  },
+  {
+    "revision": "7fe6e1212d46a21923de931e28547aec",
+    "url": "/ethmon-redesign/static/media/icon_emont.7fe6e121.png"
+  },
+  {
+    "revision": "52359f8ad725e7c001d1e5befc79137b",
+    "url": "/ethmon-redesign/static/media/icon_leaderboard_regular.52359f8a.png"
+  },
+  {
+    "revision": "49f2c2f9088883a1d022a18ef58f966d",
+    "url": "/ethmon-redesign/static/media/icon_mymon_regular.49f2c2f9.png"
+  },
+  {
+    "revision": "3fdde15daad03794745cf86e74bccc15",
+    "url": "/ethmon-redesign/static/media/icon_quest_regular.3fdde15d.png"
+  },
+  {
+    "revision": "13957e8cdbe2bec4fc821be5ffa6502e",
+    "url": "/ethmon-redesign/static/media/insta.13957e8c.png"
+  },
+  {
+    "revision": "f11757e0fc8c45da206083ac86d19d2c",
+    "url": "/ethmon-redesign/static/media/logo_mon.f11757e0.png"
+  },
+  {
+    "revision": "d285c3d242d1b653a8ea9db4366655a1",
+    "url": "/ethmon-redesign/static/media/logo_text.d285c3d2.png"
+  },
+  {
+    "revision": "e22a717f1e7bc1d69c35ad3de57de600",
+    "url": "/ethmon-redesign/static/media/medium.e22a717f.png"
+  },
+  {
+    "revision": "1a77fbd8e8d12da2d6b1524a4385c419",
+    "url": "/ethmon-redesign/static/media/page_background_1.1a77fbd8.jpg"
+  },
+  {
+    "revision": "71cba0ef56060d98b8c86fd1a221fa97",
+    "url": "/ethmon-redesign/static/media/share.71cba0ef.png"
+  },
+  {
+    "revision": "b026a63067e5bf4be71d827a23411f07",
+    "url": "/ethmon-redesign/static/media/south-korea.b026a630.png"
+  },
+  {
+    "revision": "78a50acd63df1a3096515fb8d60326c8",
+    "url": "/ethmon-redesign/static/media/telegram.78a50acd.png"
+  },
+  {
+    "revision": "0b0d94bce64e5038490b3c7defe59b2b",
+    "url": "/ethmon-redesign/static/media/twitter.0b0d94bc.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/ethmon-redesign/static/media/united-kingdom.6f382b2d.png"
+  },
+  {
+    "revision": "fe6804314ec86ea9da41329cfd63cea8",
+    "url": "/ethmon-redesign/static/media/vietnam.fe680431.png"
+  },
+  {
+    "revision": "23cfc9b8e0823001a1cb1989e552ea38",
+    "url": "/ethmon-redesign/static/media/world.23cfc9b8.png"
+  },
+  {
+    "revision": "84562a6e5f54c7b76685a35f2cdff465",
+    "url": "/ethmon-redesign/static/media/youtube.84562a6e.png"
+  }
+]);
